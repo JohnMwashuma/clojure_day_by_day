c6ef6bb54d81fac2be08f21cbdd4e87a7cfe04d7
John Mwashuma <jmwashuma@live.com>
2017-09-13 10:42:54 +0300

    Add and, or and first function calls

--- src/clojure_day_by_day/core.clj
+++ src/clojure_day_by_day/core.clj
@@ -4,3 +4,16 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+;; or Returns the first truthy value
+((or + -) 1 2 3)
+; 6
+
+;; and Returns the first falsey value or the last truthy value
+((and (= 1 1) +) 1 2 3)
+; 6
+
+;; or Returns the first element in a sequence
+((first [+ 0]) 1 2 3)
+; 6
+
