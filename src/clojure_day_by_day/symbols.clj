ca54315cea1c341beb16acf27f6c761e821954c7
John Mwashuma <jmwashuma@live.com>
2017-09-15 12:40:25 +0300

    add package name and implement require, set

--- src/clojure_day_by_day/symbols.clj
+++ src/clojure_day_by_day/symbols.clj
@@ -1,3 +1,5 @@
+(ns clojure-day-by-day.symbols)
+
 ;; Symbols
 
 (def developer "John Mwashuma") ;developer is a symbol
@@ -17,5 +19,16 @@ developer
 company
 ;=>CompilerException java.lang.RuntimeException: Unable to resolve symbol: company in this context
 
+;; Implementing require, set and let
 
+(require '[clojure.set :as s])
+
+(defn share_name [list1 list2]
+        (let [my_names (set list1)
+              other_names (set list2)
+              common_name (s/intersection my_names other_names)]
+              (str "Our common name is: " common_name)))
+
+(share_name [:john :Mwashuma][:silvan :Mwashuma])
+;=>"Our common name is: #{:Mwashuma}"
 
